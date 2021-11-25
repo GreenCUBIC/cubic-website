da70a71eb68882b4c6579bbd11ca986d783eb391
Francois <{EMAIL}>
2021-11-25 12:54:38 -0500

    Remove automatic news cycling

--- src/components/NewsBar.tsx
+++ src/components/NewsBar.tsx
@@ -5,16 +5,52 @@ import * as styles from "./NewsBar.module.scss";
 
 const NewsBar = (news) => {
   const [selectedIndex, setIndex] = useState(0);
-  setInterval(() => {
-    if (selectedIndex === news.news.length - 1) {
-      setIndex(0);
-    } else {
-      setIndex(selectedIndex + 1);
-    }
-  }, 10000);
   return (
-    <div className={styles.newsbar}>
-      <b>{news.news[selectedIndex].date}:</b> {news.news[selectedIndex].text}
+    <div className={styles.wrapper}>
+      <div className={styles.newsbar}>
+        <div
+          style={{ cursor: "pointer" }}
+          onClick={() =>
+            setIndex(
+              selectedIndex === 0 ? news.news.length - 1 : selectedIndex - 1
+            )
+          }
+        >
+          <i
+            className={`fas fa-chevron-left`}
+            style={{ height: 20, width: 20 }}
+          ></i>
+        </div>
+        <div>
+          <b>{news.news[selectedIndex].date}:</b>{" "}
+          {news.news[selectedIndex].text}
+        </div>
+        <div
+          onClick={() =>
+            setIndex(
+              selectedIndex === news.news.length - 1 ? 0 : selectedIndex + 1
+            )
+          }
+        >
+          <i
+            style={{ cursor: "pointer" }}
+            className={`fas fa-chevron-right`}
+            style={{ height: 20, width: 20 }}
+          ></i>
+        </div>
+      </div>
+      <div className={styles.circlesBar}>
+        {news.news.map((news, i) => (
+          <div onClick={() => setIndex(i)} style={{ cursor: "pointer" }}>
+            <i
+              className={`fa${selectedIndex === i ? "s" : "r"} fa-circle ${
+                styles.circle
+              }`}
+              style={{ height: 5, width: 5 }}
+            ></i>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
